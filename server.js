717483b1877e343a1c22bd9bedcf4936de2c729b
amit-singhs <amitdrive003@gmail.com>
2025-07-11 00:50:26 +0530

    - Test a new route

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 const app = require('./api/app');
 const PORT = process.env.PORT || 3000;
+app.get('/', (req, res) => {
+  res.send('Hello Universe !');
+});
 app.listen(PORT, () => {
   console.log(`Server started on http://localhost:${PORT}/api/`);
 }); 
