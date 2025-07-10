da0e907223162176ec86a090ce711ae32c3d0cff
amit-singhs <amitdrive003@gmail.com>
2025-07-11 00:47:25 +0530

    - Attempt to fix the vercel deployment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const app = require('./api/app');
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Local server running on http://localhost:${PORT}/api/`);
+  console.log(`Server started on http://localhost:${PORT}/api/`);
 }); 
